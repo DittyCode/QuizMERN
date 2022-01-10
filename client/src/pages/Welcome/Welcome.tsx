ea022e3967b174beb84234c7798bf50dab319194
Kamil <kam.nas21@wp.pl>
2022-01-10 21:39:21 +0100

    Working with welcome page, scss coding

--- client/src/pages/Welcome/Welcome.tsx
+++ client/src/pages/Welcome/Welcome.tsx
@@ -5,6 +5,11 @@ const Welcome = () => {
 	return (
 		<section className={styled.welcome}>
 			<Navbar />
+			<div className={styled.container}>
+				<h1 className={styled.welcome__title}>Quiz App</h1>
+				<p className={styled.welcome__description}>Log In to access</p>
+				<div className={styled.bubble}></div>
+			</div>
 		</section>
 	);
 };
