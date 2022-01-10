b4f50acfbf72cd49544e073e75bd084288c48967
Kamil <kam.nas21@wp.pl>
2022-01-10 14:58:31 +0100

    Add morgan to project

--- server.ts
+++ server.ts
@@ -1,12 +1,21 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import morgan from 'morgan';
 
 dotenv.config({ path: './config/.env' });
 
 const app = express();
 
+const PROJECT_MODE = process.env.MODE_ENV;
+
+if (PROJECT_MODE === 'development') {
+	app.use(morgan('dev'));
+}
+
 const SERVER_PORT = process.env.SERVER_PORT || 3000;
 
 app.listen(SERVER_PORT, () => {
-	console.log(`Server is listening at port : ${SERVER_PORT}`);
+	console.log(
+		`Server is listening at port : ${SERVER_PORT} - MODE : ${PROJECT_MODE}`
+	);
 });
